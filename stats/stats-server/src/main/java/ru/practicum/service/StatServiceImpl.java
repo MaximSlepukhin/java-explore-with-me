db59f988a4b2c43b5056218146dc9e3f756797ea
Maxim Slepukhin <slepuhinmaksym@yandex.ru>
2024-09-16 22:03:28 +0300

    Исправлены chekstyle ошибки.

--- stats/stats-server/src/main/java/ru/practicum/service/StatServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/service/StatServiceImpl.java
@@ -11,7 +11,6 @@ import ru.practicum.model.ViewStats;
 import ru.practicum.repository.StatRepository;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
