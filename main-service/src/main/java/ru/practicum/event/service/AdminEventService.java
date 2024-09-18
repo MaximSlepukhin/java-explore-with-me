c27f8381f609a50a6297e9c99d764fe175f233c9
Maxim Slepukhin <slepuhinmaksym@yandex.ru>
2024-09-18 22:57:05 +0300

    Исправлены ошибки.

--- main-service/src/main/java/ru/practicum/event/service/AdminEventService.java
+++ main-service/src/main/java/ru/practicum/event/service/AdminEventService.java
@@ -11,6 +11,5 @@ public interface AdminEventService {
 
     List<EventFullDto> getEventsForAdmin(Pageable pageable, Integer offset, Integer size, List<Long> usersIds,
                                          List<String> states, List<Long> categoriesIds, LocalDateTime rangeEnd, LocalDateTime rangeStart);
-
     EventFullDto updateEvent(Long eventId, UpdateEventAdminRequest updateEventAdminRequest);
 }
