f1b47946793345256ecddcbeab7d710a09f155df
Maxim Slepukhin <slepuhinmaksym@yandex.ru>
2024-09-23 02:14:49 +0300

    Исправлены ошибки.

--- main-service/src/main/java/ru/practicum/event/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/event/repository/EventRepository.java
@@ -3,6 +3,7 @@ package ru.practicum.event.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.event.model.Event;
