0ee0f07a00e47e6da02d8b878d8cd985f89361eb
Maxim Slepukhin <slepuhinmaksym@yandex.ru>
2024-09-26 02:06:15 +0300

    Исправлены ошибки.

--- stats/stats-server/src/main/java/ru/practicum/service/StatServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/service/StatServiceImpl.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
-import ru.practicum.exception.IncorrectDataException;
 import ru.practicum.mapper.StatsMapper;
 import ru.practicum.model.EndpointHit;
 import ru.practicum.model.ViewStats;
@@ -36,7 +35,7 @@ public class StatServiceImpl implements StatService {
         List<String> listOfUris = new ArrayList<>();
 
         if (start.isAfter(end)) {
-            throw new IncorrectDataException("Некорректный диапазон выгрузки статистики.");
+            throw new RuntimeException();
         }
         if (uri == null) {
             if (unique) {
